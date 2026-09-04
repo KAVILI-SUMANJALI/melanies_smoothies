624325b0b8697879b46e9d9aa8765c5f074d6c2d
KAVILI-SUMANJALI <kavilisumanjali@gmail.com>
2026-09-04 22:58:22 +0530

    Update streamlit_app.py
    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,11 @@ if ingredients:
     st.write(my_insert_stmt)
 
     submit = st.button("Submit Order")
-    import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+    
 
     if submit:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+        import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
